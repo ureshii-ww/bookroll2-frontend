854d0cd0da627a713ed214607e26217976009c8d
ureshii-ww <63833217+ureshii-ww@users.noreply.github.com>
2022-01-30 22:33:46 +0700

    MainButton component

---
+++ src/components/UI/MainButton/MainButton.tsx
@@ -0,0 +1,15 @@
+import React, { ButtonHTMLAttributes, FC } from 'react';
+
+interface MainButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
+
+}
+
+const MainButton: FC<MainButtonProps> = ({className, children, ...rest}) => {
+  return (
+    <button {...rest} className={`${className ? 'main-button ' + className : 'main-button'}`}>
+      {children}
+    </button>
+  );
+};
+
+export default MainButton;
