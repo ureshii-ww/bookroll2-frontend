95b1f7ffa13f1022af7124459d33b96552da8a6a
ureshii-ww <63833217+ureshii-ww@users.noreply.github.com>
2022-02-20 03:29:51 +0700

    Fixes

--- src/components/UI/SubmitButton/SubmitButton.tsx
+++ src/components/UI/SubmitButton/SubmitButton.tsx
@@ -1,16 +1,15 @@
 import React, { FC, ButtonHTMLAttributes } from 'react';
+import './submit-button.scss';
 
-export interface SubmitButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+export interface SubmitButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
 
-}
-
-const SubmitButton: FC<SubmitButtonProps> = (props) => {
+const SubmitButton: FC<SubmitButtonProps> = props => {
   const { className, children, ...rest } = props;
   return (
-    <button type="button" {...rest} className={`${className ? 'submit-button ' + className : 'submit-button'}`}>
+    <button type="submit" {...rest} className={`${className ? 'submit-button ' + className : 'submit-button'}`}>
       {children}
     </button>
   );
-}
+};
 
 export default SubmitButton;
