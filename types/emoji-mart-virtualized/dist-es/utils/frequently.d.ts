dbb9d3bc70dfc49a5903f235eb68bc3f453e59c8
ureshii-ww <63833217+ureshii-ww@users.noreply.github.com>
2022-02-20 03:26:12 +0700

    Added emoji-picker-virtualized and types for it

---
+++ types/emoji-mart-virtualized/dist-es/utils/frequently.d.ts
@@ -0,0 +1,8 @@
+import { EmojiData } from '../index';
+
+declare const _default: {
+    add(emoji: Pick<EmojiData, 'id'>): void;
+    get(perLine: number): string[];
+};
+
+export { _default as default };
