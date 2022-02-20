f29d264f4dc22de3039b988a87f21bbb1bf153ce
ureshii-ww <63833217+ureshii-ww@users.noreply.github.com>
2022-02-20 18:53:22 +0700

    fix

--- src/components/Containers/ClubSettingsForm/useClubSettingsForm.ts
+++ src/components/Containers/ClubSettingsForm/useClubSettingsForm.ts
@@ -1,19 +1,16 @@
 import { useActions } from '../../../hooks/useActions';
-import { useNavigate } from 'react-router-dom';
 import { useRequestPage } from '../../../hooks/useRequestPage';
 import ClubService from '../../../services/club.service';
-import { RouteNames } from '../../../routes/route-names.enum';
 
 const useClubSettingsForm = () => {
-  const { addNotification } = useActions();
-  const navigate = useNavigate();
+  const { addNotification, closeModal } = useActions();
 
   const sendSettingsData = useRequestPage(
     async (clubUrl: string, clubname: string, masterUrl: string, description: string) => {
       const response = await ClubService.updateSettings(clubUrl, clubname, masterUrl, description);
       if (response.data === 'Success') {
         addNotification('Настройки успешно обновлены', 'success');
-        navigate(`${RouteNames.CLUB_PROFILE_BASE}${clubUrl}`);
+        closeModal();
       }
     }
   );
