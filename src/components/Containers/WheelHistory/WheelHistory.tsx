cbe2bc166150a709c63bf104dc14256ac31634c4
ureshii-ww <63833217+ureshii-ww@users.noreply.github.com>
2022-02-23 02:27:14 +0700

    wheel history styling

--- src/components/Containers/WheelHistory/WheelHistory.tsx
+++ src/components/Containers/WheelHistory/WheelHistory.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 import { WheelWinnerInfo } from '../../../models/wheel-winner-info';
 import useWheelHistory from './useWheelHistory';
 import TransparentButton from '../../UI/TransparentButton/TransparentButton';
+import './wheel-history.scss';
 
 interface WheelHistoryProps {
   rollsHistory: WheelWinnerInfo[];
@@ -11,12 +12,19 @@ const WheelHistory: FC<WheelHistoryProps> = ({ rollsHistory, ...rest }) => {
   const confirmBook = useWheelHistory(rollsHistory);
 
   return (
-    <div>
+    <div className="wheel-history">
       {rollsHistory.map((winner, index) => (
-        <div>
-          <div>{index + 1}. Книга от {winner.user.username}</div>
-          <div>{winner.book.title} - {winner.book.authors.join(', ')}</div>
-          <TransparentButton onClick={() => confirmBook(index)}>Подтвердить книгу</TransparentButton>
+        <div className="wheel-history__group">
+          <div className="wheel-history__user">
+            {index + 1}. Книга от <span className="wheel-history__username">{winner.user.username}</span>{' '}
+            <span className="wheel-history__emoji">{winner.user.emoji}</span>
+          </div>
+          <div className="wheel-history__book">
+            <span className="wheel-history__title">{winner.book.title}</span> - {winner.book.authors.join(', ')}
+          </div>
+          <div className="wheel-history__button">
+            <TransparentButton onClick={() => confirmBook(index)}>Подтвердить книгу</TransparentButton>
+          </div>
         </div>
       ))}
     </div>
