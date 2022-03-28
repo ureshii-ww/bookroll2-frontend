363fdd65fcb82af6cf2d9fbe2d2493f6ea98b144
ureshii-ww <63833217+ureshii-ww@users.noreply.github.com>
2022-03-29 01:58:26 +0700

    fix...

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React, { FC, Fragment } from 'react';
+import React, { FC, Fragment, useEffect } from 'react';
 import AppRouter from './components/AppRouter/AppRouter';
 import './index.scss';
 import NotificationsContainer from './components/Containers/NotificationsContainer/NotificationsContainer';
 import Modal from './components/Containers/Modal/Modal';
 
 const App: FC = () => {
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+  }, []);
+
   return (
     <Fragment>
       <AppRouter />
